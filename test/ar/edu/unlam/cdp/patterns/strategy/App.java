2599208c5d5fbe4147e96d739b8cde1746692848
Lucas Videla <videla.lucas@gmail.com>
2012-07-14 00:36:47 -0300

    Se agrega patrón Strategy

---
+++ test/ar/edu/unlam/cdp/patterns/strategy/App.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.cdp.patterns.strategy;
+
+import org.junit.Test;
+
+public class App {
+
+
+	@Test
+	public void testStrategy(){
+
+		Report report1 = new Report(new HTMLFormatter());
+		report1.outputReport();
+		
+		Report report2 = new Report(new PlainTextFormatter());
+		report2.outputReport();
+		
+	}
+
+}
